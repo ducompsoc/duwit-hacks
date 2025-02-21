00dd94c1d03f51e5191550e67de1b33b6b5a1d94
AMuh2020 <leasu.muhmeen@gmail.com>
2025-02-21 15:19:52 +0000

    feat: /sign-up redirects to the sign up google form

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/sign-up",
+        destination: "https://forms.gle/ngDdviKZJ6RfBvHZ8",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
